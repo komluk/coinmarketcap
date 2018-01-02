3832b2395699c6ea941776c347f94e935e6a61a8
Łukasz Komosa <komluk@hotmail.com>
2018-01-02 18:10:48 +0100

    Blockchain wallet

---
+++ src/Services/BlockchainService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Services.Contracts;
+
+namespace Services
+{
+    public class BlockchainService
+    {
+
+    }
+}
